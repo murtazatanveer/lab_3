ea382640a725d949963d65e8c8fbbe561637f007
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-19 18:38:36 +0500

    1st commit in year.c

--- leapYear.c
+++ leapYear.c
@@ -62,6 +62,17 @@ int main(int argc, char **argv) {
   //TODO: write *at least* 3 more of your own
   //      test cases here, they should all pass!
 
+
+  year = 2008;
+  printf("Test Case 3: year = %d: ", year);
+  if(isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
   printf("\n\n");
   printf("Summary:\n");
   printf("Number of test cases passed: %d\n", numPassed);
