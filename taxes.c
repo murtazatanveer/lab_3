e6e2eb3c961528bec5e8887a4d32fd6a86667270
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-22 14:39:32 +0500

    9th

--- taxes.c
+++ taxes.c
@@ -27,10 +27,12 @@ int main(int argc, char **argv) {
   printf("\nPlease enter your adjusted gross income (AGI): ");
   scanf("%lf", &agi);
   //remove the "enter" endline character
-  
+
+  getchar();
 
   printf("\n\nDo you have any children? (Y) or (N)? : ");
-  scanf("%c",&c);
+
+  c=getchar();
 
   if(c == 'y' || c == 'Y') {
 
@@ -46,7 +48,7 @@ if(agi>0 && agi<=19900  ){
 
   tax = (10*agi)/(100);
   
-  totalTax = tax - (childCredit);
+  totalTax = tax - childCredit;
 
  
 }
@@ -56,6 +58,8 @@ else if(agi>=19901 && agi<=81050){
  calculation = (agi-19900);
 
  tax = ((12*calculation)/100) + 1990;
+
+  totalTax = tax - childCredit;
 
 }
 
@@ -68,6 +72,7 @@ else if(agi>=81051 && agi<=172750){
 
  tax = ((22*calculation)/100) + 9328;
 
+ totalTax = tax - childCredit;
 }
 
 
@@ -77,6 +82,7 @@ else if(agi>=172751 && agi<=329850){
 
  tax = ((24*calculation)/100) + 29502;
 
+ totalTax = tax - childCredit;
 }
 
 
@@ -85,6 +91,8 @@ else if(agi>=329851 && agi<=418850){
  calculation = (agi-329850);
 
  tax = ((32*calculation)/100) + 67206;
+
+  totalTax = tax - childCredit;
 
 }
 
@@ -95,6 +103,8 @@ else if(agi>=418851 && agi<=628300){
 
  tax = ((35*calculation)/100) + 95686;
 
+  totalTax = tax - childCredit;
+
 }
 
 else if(agi>=628301){
@@ -103,6 +113,8 @@ else if(agi>=628301){
 
  tax = ((37*calculation)/100) + 168933.50;
 
+  totalTax = tax - childCredit;
+
 }
 
 
@@ -110,7 +122,7 @@ else if(agi>=628301){
 
   //TODO: compute the tax, child credit, and total tax here
 
- printf("AGI:          $%.2lf\n\n", agi);
+ printf("\n\nAGI:          $%.2lf\n\n", agi);
   printf("Tax:          $%.2lf\n\n", tax);
   printf("Child Credit: $%.2lf\n\n", childCredit);
   printf("Total Tax:    $%.2f\n\n", totalTax);
